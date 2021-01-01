ec2540b27a9c53011839b2c63f912fa72b79a276
Saurav Adhikari <sauravads123@gmail.com>
2021-01-01 14:50:59 +0545

    improv : working with JSON

--- main.go
+++ main.go
@@ -1,23 +1,64 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-// Using http.Handle
-type fooHandler struct {
-	Message string
+// Product
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productsJson := `[
+		{
+		  "productId": 1,
+		  "manufacturer": "Johns-Jenkins",
+		  "sku": "p5z343vdS",
+		  "upc": "939581000000",
+		  "pricePerUnit": "497.45",
+		  "quantityOnHand": 9703,
+		  "productName": "sticky note"
+		},
+		{
+		  "productId": 2,
+		  "manufacturer": "Hessel, Schimmel and Feeney",
+		  "sku": "i7v300kmx",
+		  "upc": "740979000000",
+		  "pricePerUnit": "282.29",
+		  "quantityOnHand": 9217,
+		  "productName": "leg warmers"
+		}]`
+	err := json.Unmarshal([]byte(productsJson), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-// Using http.HandleFunc
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Bar called"))
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJSON, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJSON)
+
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo Called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
